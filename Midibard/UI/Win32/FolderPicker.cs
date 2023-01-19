3c2c6af3970b8eabf00fdc3378463107d94335ec
akira0245 <33274390+akira0245@users.noreply.github.com>
2023-01-19 13:30:15 +0800

    update file header

--- Midibard/UI/Win32/FolderPicker.cs
+++ Midibard/UI/Win32/FolderPicker.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿// Copyright (C) 2022 akira0245
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see https://github.com/akira0245/MidiBard/blob/master/LICENSE.
+// 
+// This code is written by akira0245 and was originally used in the MidiBard project. Any usage of this code must prominently credit the author, akira0245, and indicate that it was originally used in the MidiBard project.
+
+using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.ComTypes;
